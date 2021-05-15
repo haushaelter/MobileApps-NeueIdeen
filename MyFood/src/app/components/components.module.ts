0ca80c2734f91e32ac1339d5d36ec42136141002
haushaelter <ahaushaelter99@gmail.com>
2021-05-15 14:47:08 +0200

    Hinzufügen von Navigationsbalken

--- MyFood/src/app/components/components.module.ts
+++ MyFood/src/app/components/components.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { SearchbarComponent } from './searchbar/searchbar.component';
 import { ListRezeptComponent } from './list-rezept/list-rezept.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 const PAGES_COMPONENTS = [
   SearchbarComponent,
-  ListRezeptComponent
+  ListRezeptComponent,
+  NavigationBarComponent
 ]
 
 @NgModule({
