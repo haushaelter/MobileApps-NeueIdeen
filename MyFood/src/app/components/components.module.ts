a7a0fc6d16639eaf91aa8585f053b788ae25a379
haushaelter <ahaushaelter99@gmail.com>
2021-05-17 14:06:32 +0200

    Fertigstellen der Navigationsfunktion

--- MyFood/src/app/components/components.module.ts
+++ MyFood/src/app/components/components.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { SearchbarComponent } from './searchbar/searchbar.component';
 import { ListRezeptComponent } from './list-rezept/list-rezept.component';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { ListKochbuchComponent } from './list-kochbuch/list-kochbuch.component';
 
 const PAGES_COMPONENTS = [
   SearchbarComponent,
   ListRezeptComponent,
-  NavigationBarComponent,
   ListKochbuchComponent
 ]
 
