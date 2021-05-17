b5556bff6a39d5f9227759fd3125fbddd2308d11
haushaelter <ahaushaelter99@gmail.com>
2021-05-17 15:35:10 +0200

    Toasts bei Registrierung

--- MyFood/src/app/register/register.page.ts
+++ MyFood/src/app/register/register.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { Logging } from '../services/helper';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,8 @@ export class RegisterPage implements OnInit {
   user: any = {};
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private logging: Logging
   ) { }
 
   ngOnInit() {
@@ -22,8 +24,12 @@ export class RegisterPage implements OnInit {
       if (this.user.passwort === this.user.passwortWiederholung) {
         this.authService.registrieren(this.user.email, this.user.passwort);
       } else {
-        console.log("Passwörter stimmen nicht überein.");
+        this.logging.zeigeToast("Passwörter stimmen nicht überein");
       }
+    } else if(this.user.email){
+      this.logging.zeigeToast("Bitte Passwort eingeben");
+    } else {
+      this.logging.zeigeToast("Bitte E-Mail eingeben");
     }
   }
 
