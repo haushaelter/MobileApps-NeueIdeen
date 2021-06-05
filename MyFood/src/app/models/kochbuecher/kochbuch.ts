8f91754fa46071a70799b1f3e6e48b3af318c3f7
haushaelter <ahaushaelter99@gmail.com>
2021-06-05 19:05:09 +0200

    Update kochbuch.ts

--- MyFood/src/app/models/kochbuecher/kochbuch.ts
+++ MyFood/src/app/models/kochbuecher/kochbuch.ts
@@ -3,6 +3,10 @@ import { Deserialize } from "../deserialize";
 export class Kochbuch implements Deserialize{
 
     id: string;
+    bewertung: {
+        anzahl: number;
+        bewertung: number;
+    };
     rezepte: Array<string>
 
     deserialize(input:any){
